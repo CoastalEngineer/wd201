58aa47328304194e9b6bc8631792ecad3b3bb277
CoastalEngineer <125397520+CoastalEngineer@users.noreply.github.com>
2023-03-06 10:06:22 +0530

    Todo-cli implementation

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -10,21 +10,38 @@ const todoList = () => {
   const overdue = () => {
     // Write the date check condition here and return the array
     // of overdue items accordingly.
+    return all.filter((value) => {
+      return value.dueDate < today;
+    });
   };
 
   const dueToday = () => {
     // Write the date check condition here and return the array
     // of todo items that are due today accordingly.
+
+    return all.filter((value) => {
+      return value.dueDate === today;
+    });
   };
 
   const dueLater = () => {
     // Write the date check condition here and return the array
     // of todo items that are due later accordingly.
+    return all.filter((value) => {
+      return value.dueDate > today;
+    });
   };
 
   const toDisplayableList = (list) => {
     // Format the To-Do list here, and return the output string
     // as per the format given above.
+    list.map((item) => {
+      return `${item.completed ? "[x]" : "[ ]"} ${item.title} ${
+        item.dueDate === today ? "" : item.dueDate
+      }\n`;
+    });
+
+    return list;
   };
 
   return {
